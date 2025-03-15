ef8c99de0e8b1fd793f9076aa7f0543f1ad51a98
DucNghia176 <nghi7a@gmail.com>
2025-03-16 02:28:45 +0700

    login

---
+++ login-service1/src/main/java/com/example/login_service1/LoginService1Application.java
@@ -0,0 +1,13 @@
+package com.example.login_service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginService1Application.class, args);
+	}
+
+}
---
+++ login-service1/src/test/java/com/example/login_service1/LoginService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login_service1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
