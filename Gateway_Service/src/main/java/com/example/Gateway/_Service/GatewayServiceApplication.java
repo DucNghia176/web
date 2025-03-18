b5e5b96eabc4a22c3c9355b42934f479e54b9c29
DucNghia176 <nghi7a@gmail.com>
2025-03-19 00:24:45 +0700

    service

--- Gateway_Service/src/main/java/com/example/Gateway/_Service/GatewayServiceApplication.java
+++ Gateway_Service/src/main/java/com/example/Gateway/_Service/GatewayServiceApplication.java
@@ -2,10 +2,12 @@ package com.example.Gateway._Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+//@EnableDiscoveryClient
 public class GatewayServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
